e9b2c81d3e0c376463d5aa7e91e39c0e70a66540
nolan freeman abbott <nolan@freemanabbott.com>
2025-03-19 00:36:25 -0500

    'add' command test

--- src/main.zig
+++ src/main.zig
@@ -2,34 +2,39 @@ const std = @import("std");
 const clap = @import("clap");
 
 // git ignore = quig
-// quig .zig-out/
-// quig config/something
-// quig - zig
+// quig add .zig-out/
+// quig add config/something
+// quig add - zig
 // quig list
 // quig list --all
-// quig --global .zig-out/
+// quig --global add .zig-out/
 // quig list --global
-// quig -u .zig-out/
+// quig remove .zig-out/
 // quig version
 
 const SubCommands = enum {
     help,
-    list,
     version,
+    list,
+    add,
+    remove,
 };
 
 const main_parsers = .{
     .command = clap.parsers.enumeration(SubCommands),
+    .str = clap.parsers.string,
 };
 
 const main_params = clap.parseParamsComptime(
-    \\-h, --help            Display help and exit.
-    \\-v, --version         Display version info and exit.
-    \\-t, --template
     \\<command>
+    \\
 );
 
-const MainArgs = clap.ResultEx(clap.Help, &main_params, main_parsers);
+const MainArgs = clap.ResultEx(
+    clap.Help,
+    &main_params,
+    main_parsers
+);
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -42,25 +47,60 @@ pub fn main() !void {
     _ = it.next();
 
     var diagnostic = clap.Diagnostic{};
-    var res = clap.parseEx(clap.Help, &main_params, main_parsers, &it, .{
-        .diagnostic = &diagnostic,
-        .allocator = allocator,
-        .terminating_positional = 0,
+
+    var res = clap.parseEx(
+        clap.Help,
+        &main_params,
+        main_parsers,
+        &it,
+        .{
+            .diagnostic = &diagnostic,
+            .allocator = allocator,
+            .terminating_positional = 0,
     }) catch |err| {
         diagnostic.report(std.io.getStdErr().writer(), err) catch {};
         return err;
     };
     defer res.deinit();
 
-    if (res.args.help != 0) {
-        std.debug.print("--help\n", .{});
-    }
-
     const command = res.positionals[0] orelse return error.MissingCommand;
     switch (command) {
         .help => std.debug.print("--help\n", .{}),
-        .list => try listMain(allocator, &it, res),
         .version => std.debug.print("Quick Ignore. Version 0.1 beta.\n", .{}),
+        .add =>  try commandAdd(allocator, &it, res),
+        .list => try listMain(allocator, &it, res),
+        else => {
+            std.debug.print("Something wrong.\n", .{});
+        },
+    }
+}
+
+fn commandAdd(allocator: std.mem.Allocator, it: *std.process.ArgIterator, main_args: MainArgs) !void {
+    _ = main_args;
+
+    const params = comptime clap.parseParamsComptime(
+        \\-t, --template <str>...  Ingore using a template.
+        \\<str>...                 Ignore specific paths
+    );
+
+    var diagnostic = clap.Diagnostic{};
+    var res = clap.parseEx(clap.Help, &params, clap.parsers.default, it, .{
+        .diagnostic = &diagnostic,
+        .allocator = allocator,
+    }) catch |err| {
+        diagnostic.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    // ??? only one of these options
+
+    for (res.args.template) |t| {
+        std.debug.print("--template = {s}\n", .{t});
+    }
+
+    for (res.positionals[0]) |path| {
+        std.debug.print("--path = {s}\n", .{path});
     }
 }
 
